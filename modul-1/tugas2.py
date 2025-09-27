4146d7e7eead363f6c3238d0e23ddc0733681f53
nafiz <nafismuzadi06@gmail.com>
2025-09-27 14:52:24 +0700

    250441100142-Moh.NafisMuzadi-AsprakKakOcta

---
+++ modul-1/tugas2.py
@@ -0,0 +1,11 @@
+panjang = int(input ("masukkan panjang balok : "))
+lebar = int(input ("masukkan lebar balok : "))
+tinggi = int(input ("masukkan tinggi balok : "))
+
+volume = panjang * lebar * tinggi
+
+luas = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi )
+
+print("volume balok adalah :", volume, "cm")
+print("luas balok adalah :", luas, "cm")
+
---
+++ modul-1/tugas3.py
@@ -0,0 +1,10 @@
+import math
+
+bola = 8 + 6
+
+diambil = 3
+
+kombinasi = math.comb(bola, diambil)
+
+print("ada", kombinasi, "kombinasi yang dapat diambil")
+
