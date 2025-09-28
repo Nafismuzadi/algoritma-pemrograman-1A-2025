ce729a87a4d968e34385735132b9a54d216ccc39
nafiz <nafismuzadi06@gmail.com>
2025-09-28 21:51:57 +0700

    250441100142-Moh.NafisMuzadi-AsprakKakOcta

--- modul-1/250441100142-Moh.NafisMuzadi3.py
+++ modul-1/250441100142-Moh.NafisMuzadi3.py
@@ -1,10 +1,10 @@
-import math
+merah = 8
+biru = 6
 
-bola = 8 + 6
-
+total = merah + biru 
 diambil = 3
 
-kombinasi = math.comb(bola, diambil)
+rumus_kombinasi = total * (total - 1) * (total - 2) // (diambil * (diambil - 1) * (diambil - 2))
 
-print("ada", kombinasi, "kombinasi yang dapat diambil")
+print("ada", rumus_kombinasi, "kemungkinan bola yang dapat diambil")
 
